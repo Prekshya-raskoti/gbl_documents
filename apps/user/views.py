e004bccd001e469322b8849c481e9c27d93a9b8e
Prekshya <bisuraskoti@gmail.com>
2025-04-03 16:38:54 +0545

    add edit and delete icon

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.contrib import messages
 from django.views.generic import ListView ,CreateView
 from .models import Vendor
 from django.urls import reverse_lazy
 from .forms import VendorForm
+
 class VendorListView(ListView):
     model = Vendor
     template_name = 'user/user_vendor_list.html'
@@ -16,8 +17,10 @@ class VendorCreateView(CreateView):
 
     def form_valid(self, form):
         response = super().form_valid(form)
+        messages.success(self.request, "Vendor has been successfully created!")
         return response
 
     def form_invalid(self, form):
+        messages.error(self.request, "There was an error with the form submission.")
         return super().form_invalid(form)    
    
