d93a8fdfe68ad08a0d7dd5e0cb4c69ddafe28752
amritab3 <shellybhattarai@gmail.com>
2025-04-07 12:18:14 +0545

    edit document model

--- apps/documents/models.py
+++ apps/documents/models.py
@@ -4,6 +4,7 @@ from  apps.user.models import Vendor
 class Document(models.Model):
     DOCUMENT_TYPES = [
         ('citizenship', 'Citizenship'),
+        ('Citizenship Back', 'Citizenship Back'),
         ('bank_details', 'Bank Details'),
         ('pan_card', 'PAN Card'),
         ('other', 'Other'),
@@ -13,6 +14,9 @@ class Document(models.Model):
     document_type = models.CharField(max_length=50, choices=DOCUMENT_TYPES)
     file = models.FileField(upload_to='vendor_documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 
     def __str__(self):
         return f"{self.vendor.name} - {self.get_document_type_display()}"
