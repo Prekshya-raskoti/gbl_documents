5385efb9255330a4ec2348bdf4450817abf4ecab
amritab3 <shellybhattarai@gmail.com>
2025-04-15 13:43:15 +0545

    changed gitignore and views

--- apps/contracts/models.py
+++ apps/contracts/models.py
@@ -4,7 +4,6 @@ from apps.user.models import Vendor
 
 class Contract(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='contracts')
-    file = models.FileField(upload_to='vendor_contracts/')
     join_date = models.DateField()
     expiry_date = models.DateField()
     terms_conditions = models.TextField()
@@ -13,3 +12,12 @@ class Contract(models.Model):
 
     def __str__(self):
         return f"Contract for {self.vendor.name} (Expires on {self.expiry_date})"
+
+class ContractFile(models.Model):
+    file = models.FileField(upload_to='vendor_contracts/')
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, related_name='files')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name="contract_files_uploaded")
+
+    def __str__(self):
+        return f"Contract document for {self.contract.vendor.name} (Expires on {self.uploaded_at})"
